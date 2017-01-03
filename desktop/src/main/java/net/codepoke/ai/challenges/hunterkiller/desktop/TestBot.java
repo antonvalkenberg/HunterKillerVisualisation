2c00fe890cd33cf7cfcb9e6115c77ab4490f1d35
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2017-01-03 15:24:25 +0100

    Quick TestBot that just returns a NullMove

--- desktop/src/main/java/net/codepoke/ai/challenges/hunterkiller/desktop/TestBot.java
+++ desktop/src/main/java/net/codepoke/ai/challenges/hunterkiller/desktop/TestBot.java
@@ -1,22 +1,20 @@
 package net.codepoke.ai.challenges.hunterkiller.desktop;
 
-import com.badlogic.gdx.utils.Array;
-
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerAction;
+import net.codepoke.ai.challenge.hunterkiller.HunterKillerState;
+import net.codepoke.ai.challenge.hunterkiller.orders.NullMove;
 import net.codepoke.ai.network.AIBot;
 
-import net.codepoke.ai.challenge.hunterkiller.HunterKillerAction;
-import net.codepoke.ai.challenge.hunterkiller.HunterKillerAction.Move;
-import net.codepoke.ai.challenge.hunterkiller.HunterKillerState;
-
-public class TestBot extends AIBot<HunterKillerState, HunterKillerAction> {
-	
-	Array<Move> potential = Array.with(Move.LEFT, Move.RIGHT, Move.DOWN, Move.UP, Move.PLACE_BOMB);
+public class TestBot
+		extends AIBot<HunterKillerState, HunterKillerAction> {
 
 	public TestBot() {
+		// TODO Create new BotUID for HunterKiller test bot
 		super("dpeo9nqfhvchpg3tf1m49ss1hd", HunterKillerState.class, HunterKillerAction.class);
 	}
 
 	public HunterKillerAction handle(HunterKillerState state) {
-		return new HunterKillerAction(potential.random(), state.getCurrentPlayer(), state.getCurrentRound());
+		// TODO Create random play bot
+		return new NullMove(state);
 	}
 }
