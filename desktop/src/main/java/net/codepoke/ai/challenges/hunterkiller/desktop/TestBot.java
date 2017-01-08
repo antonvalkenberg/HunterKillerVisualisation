109b9ccd6ecd15b91ab3588fc324c10ac21735e3
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2017-01-09 00:17:16 +0100

    Used MoveGenerator to do random moves.

--- desktop/src/main/java/net/codepoke/ai/challenges/hunterkiller/desktop/TestBot.java
+++ desktop/src/main/java/net/codepoke/ai/challenges/hunterkiller/desktop/TestBot.java
@@ -1,14 +1,15 @@
 package net.codepoke.ai.challenges.hunterkiller.desktop;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import net.codepoke.ai.challenge.hunterkiller.HunterKillerAction;
 import net.codepoke.ai.challenge.hunterkiller.HunterKillerState;
+import net.codepoke.ai.challenge.hunterkiller.MoveGenerator;
 import net.codepoke.ai.challenge.hunterkiller.Player;
-import net.codepoke.ai.challenge.hunterkiller.enums.UnitOrderType;
+import net.codepoke.ai.challenge.hunterkiller.gameobjects.mapfeature.Base;
 import net.codepoke.ai.challenge.hunterkiller.gameobjects.unit.Unit;
+import net.codepoke.ai.challenge.hunterkiller.orders.BaseOrder;
 import net.codepoke.ai.challenge.hunterkiller.orders.UnitOrder;
 import net.codepoke.ai.network.AIBot;
 
@@ -16,6 +17,8 @@ public class TestBot
 		extends AIBot<HunterKillerState, HunterKillerAction> {
 
 	private static Random r = new Random();
+	private static final double noUnitOrderThreshold = 0.2;
+	private static final double noBaseOrderThreshold = 0.5;
 
 	public TestBot() {
 		// TODO Create new BotUID for HunterKiller test bot
@@ -23,23 +26,36 @@ public class TestBot
 	}
 
 	public HunterKillerAction handle(HunterKillerState state) {
-		// TODO Create random play bot
-		int actionIndex = 0;
-
+		// Create a random action
 		HunterKillerAction random = new HunterKillerAction(state);
-
 		Player player = state.getPlayer(state.getCurrentPlayer());
 
-		for (Unit unit : player.getUnits(state.getMap())) {
-			List<UnitOrder> rotations = new ArrayList<UnitOrder>();
-			rotations.add(new UnitOrder(unit, UnitOrderType.ROTATE_EAST, actionIndex));
-			rotations.add(new UnitOrder(unit, UnitOrderType.ROTATE_WEST, actionIndex));
+		// Check if we want to order the base to do something
+		if (r.nextDouble() >= noBaseOrderThreshold) {
+			// Get all legal orders for this base
+			List<BaseOrder> legalOrders = MoveGenerator.getAllLegalOrders(state, (Base) state.getMap()
+																								.getObject(player.getBaseID()));
 
-			random.addOrder(rotations.get(r.nextInt(rotations.size())));
-
-			actionIndex++;
+			// Add a random order
+			if (!legalOrders.isEmpty())
+				random.addOrder(legalOrders.get(r.nextInt(legalOrders.size())));
 		}
 
+		// Move through all Units
+		for (Unit unit : player.getUnits(state.getMap())) {
+			// Check if we want to do nothing
+			if (r.nextDouble() <= noUnitOrderThreshold)
+				continue;
+
+			// Get all legal orders for this unit
+			List<UnitOrder> legalOrders = MoveGenerator.getAllLegalOrders(state, unit);
+
+			// Add a random order
+			if (!legalOrders.isEmpty())
+				random.addOrder(legalOrders.get(r.nextInt(legalOrders.size())));
+		}
+
+		// Return the randomly created action
 		return random;
 	}
 
